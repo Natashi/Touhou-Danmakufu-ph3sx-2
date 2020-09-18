8db4331c1dee7d08380a7c22a9b8665e2e7ad84c
Natashi <32347635+Natashi@users.noreply.github.com>
2020-09-18 19:22:54 +0700

    Removed DirectX9c, DX headers and libraries are now dependent on the official DirectX SDK

--- source/GcLib/pch.h
+++ source/GcLib/pch.h
@@ -7,8 +7,31 @@
 
 //------------------------------------------------------------------------------
 
-//--------------------------------Force Unicode---------------------------------
 
+
+//----------------------------Warning Suppressions-------------------------------
+
+#pragma warning (disable : 4786)	//__declspec attributes before linkage specification are ignored
+#pragma warning (disable : 4018)	//signed/unsigned mismatch
+#pragma warning (disable : 4244)	//conversion from 'x' to 'y', possible loss of data
+#pragma warning (disable : 4503)	//decorated name length exceeded, name was truncated
+
+#pragma warning (disable : 4302)	//truncation from 'x' to 'y'
+#pragma warning (disable : 4305)	//initializing/argument: truncation from 'x' to 'y'
+#pragma warning (disable : 4819)	//character in file can't be represented in the current code page, save file in Unicode
+#pragma warning (disable : 4996)	//deprecated code
+
+#pragma warning (disable : 26451)	//arithmetic overflow : ..... (io.2)
+#pragma warning (disable : 26495)	//variable x is uninitialized
+#pragma warning (disable : 26812)	//prefer 'enum class' over 'enum'.
+
+//------------------------------------------------------------------------------
+
+//Minimum OS -> Windows 7
+#define WINVER _WIN32_WINNT_WIN7
+#define _WIN32_WINNT _WIN32_WINNT_WIN7
+
+//Force Unicode
 #ifdef _MBCS
 #undef _MBCS
 #endif
@@ -19,40 +42,6 @@
 #define _UNICODE
 #endif
 
-//------------------------------------------------------------------------------
-
-//Minimum OS -> Windows 7
-#define WINVER _WIN32_WINNT_WIN7
-#define _WIN32_WINNT _WIN32_WINNT_WIN7
-
-//------------------------------Windows Libraries-------------------------------
-
-#pragma comment (lib, "winmm.lib")
-#pragma comment (lib, "comctl32.lib")
-#pragma comment (lib, "pdh.lib")
-#pragma comment (lib, "gdi32.lib")
-#pragma comment (lib, "shlwapi.lib")
-#pragma comment (lib, "psapi.lib")
-
-//----------------------------Warning Suppressions-------------------------------
-
-#pragma warning (disable : 4786) //__declspec attributes before linkage specification are ignored
-#pragma warning (disable : 4018) //signed/unsigned mismatch
-#pragma warning (disable : 4244) //conversion from 'x' to 'y', possible loss of data
-#pragma warning (disable : 4503) //decorated name length exceeded, name was truncated
-
-#pragma warning (disable : 4302) //truncation from 'x' to 'y'
-#pragma warning (disable : 4305) //initializing/argument: truncation from 'x' to 'y'
-#pragma warning (disable : 4819) //character in file can't be represented in the current code page, save file in Unicode
-#pragma warning (disable : 4996) //deprecated code
-
-#pragma warning (disable : 26451) //arithmetic overflow : ..... (io.2)
-#pragma warning (disable : 26495) //variable x is uninitialized
-#pragma warning (disable : 26812) //prefer 'enum class' over 'enum'.
-
-//------------------------------------------------------------------------------
-
-//define
 #ifndef STRICT
 #define STRICT 1
 #endif
@@ -72,6 +61,13 @@
 #include <mlang.h>
 #include <psapi.h>
 
+#pragma comment (lib, "winmm.lib")
+#pragma comment (lib, "comctl32.lib")
+#pragma comment (lib, "pdh.lib")
+#pragma comment (lib, "gdi32.lib")
+#pragma comment (lib, "shlwapi.lib")
+#pragma comment (lib, "psapi.lib")
+
 //-----------------------------------DirectX------------------------------------
 
 #define D3D_OVERLOADS
@@ -89,8 +85,6 @@
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxerr.lib")
 
-#endif
-
 #if defined(DNH_PROJ_EXECUTOR)
 
 #define DIRECTSOUND_VERSION 0x0900
@@ -102,15 +96,15 @@
 #include <basetsd.h>
 #include <d3dx9.h>
 #include <dsound.h>
-#include <dmusici.h>
 
 #pragma comment(lib, "msacm32.lib")
 #pragma comment(lib, "d3dx9.lib")
-#pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dsound.lib")
 #pragma comment(lib, "d3dxof.lib")
 
-#endif
+#endif	//defined(DNH_PROJ_EXECUTOR)
+
+#endif	//defined(DNH_PROJ_EXECUTOR) || defined(DNH_PROJ_CONFIG)
 
 //------------------------------------------------------------------------------
 
