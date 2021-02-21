f1342e0d85d1457f2f0a2617508b456dec120806
Natashi <32347635+Natashi@users.noreply.github.com>
2021-02-21 15:01:20 +0700

    Version update

--- source/TouhouDanmakufu/Common/DnhConstant.hpp
+++ source/TouhouDanmakufu/Common/DnhConstant.hpp
@@ -9,4 +9,4 @@ using namespace directx;
 constexpr const int STANDARD_FPS = 60;
 
 const std::wstring DNH_EXE_DEFAULT = L"th_dnh_ph3sx.exe";
-const std::wstring DNH_VERSION = L"v1.20b";
+const std::wstring DNH_VERSION = L"v1.21a-pre";
