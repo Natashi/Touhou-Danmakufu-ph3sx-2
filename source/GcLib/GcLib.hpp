85e35627047a3443ba0a0dd4dcfeb2ce87b306d2
Naudiz <31663251+nazjun@users.noreply.github.com>
2021-06-16 17:56:17 -0400

    Output file name update

--- source/GcLib/GcLib.hpp
+++ source/GcLib/GcLib.hpp
@@ -7,7 +7,7 @@
 
 constexpr const int STANDARD_FPS = 60;
 
-const std::wstring DNH_EXE_NAME = L"th_dnh_ph3sx.exe";
+const std::wstring DNH_EXE_NAME = L"th_dnh_ph3sx-zlabel.exe";
 const std::wstring DNH_VERSION = L"v1.22a-pre";
 
 constexpr const uint64_t _GAME_VERSION_RESERVED = /*e*/621;		//OWO!!!!!
