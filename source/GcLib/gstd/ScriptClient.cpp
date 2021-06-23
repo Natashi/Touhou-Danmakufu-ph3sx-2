4a368c5c40ac893714c26402b0c8c748882b05b1
Naudiz <31663251+nazjun@users.noreply.github.com>
2021-06-22 21:56:45 -0400

    Dunno why that was there

--- source/GcLib/gstd/ScriptClient.cpp
+++ source/GcLib/gstd/ScriptClient.cpp
@@ -1355,8 +1355,6 @@ value ScriptClientBase::Func_Interpolate_Array(script_machine* machine, int argc
 }
 
 value ScriptClientBase::Func_Rotate2D(script_machine* machine, int argc, const value* argv) {
-	ScriptClientBase* script = reinterpret_cast<ScriptClientBase*>(machine->data);
-
 	double xo = argv[2].as_real();
 	double yo = argv[3].as_real();
 	
