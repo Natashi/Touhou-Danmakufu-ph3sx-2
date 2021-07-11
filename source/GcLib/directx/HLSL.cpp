07247fb4173306d51e8ed1d03f462faf5032591a
Natashi <32347635+Natashi@users.noreply.github.com>
2021-07-11 13:57:20 +0700

    Fixed nullptr read crashes, and removed unused stuff

--- source/GcLib/directx/HLSL.cpp
+++ source/GcLib/directx/HLSL.cpp
@@ -6,74 +6,6 @@
 using namespace gstd;
 
 namespace directx {
-	const std::string ShaderSource::nameSkinnedMesh_ = "_HLSL_INTERNAL_SKINNED_MESH";
-	const std::string ShaderSource::sourceSkinnedMesh_ =
-		"float4 lightDirection;"
-		"float4 materialAmbient : MATERIALAMBIENT;"
-		"float4 materialDiffuse : MATERIALDIFFUSE;"
-		"float fogNear;"
-		"float fogFar;"
-		"static const int MAX_MATRICES = 80;"
-		"float4x3 mWorldMatrixArray[MAX_MATRICES] : WORLDMATRIXARRAY;"
-		"float4x4 mViewProj : VIEWPROJECTION;"
-
-		"struct VS_INPUT {"
-			"float4 pos : POSITION;"
-			"float4 blendWeights : BLENDWEIGHT;"
-			"float4 blendIndices : BLENDINDICES;"
-			"float3 normal : NORMAL;"
-			"float2 tex0 : TEXCOORD0;"
-		"};"
-
-		"struct VS_OUTPUT {"
-			"float4 pos : POSITION;"
-			"float4 diffuse : COLOR;"
-			"float2 tex0 : TEXCOORD0;"
-			"float fog : FOG;"
-		"};"
-
-		"float3 CalcDiffuse(float3 normal) {"
-			"float res;"
-			"res = max(0.0f, dot(normal, lightDirection.xyz));"
-			"return (res);"
-		"}"
-
-		"VS_OUTPUT DefaultTransform(VS_INPUT i, uniform int numBones) {"
-			"VS_OUTPUT o;"
-			"float3 pos = 0.0f;"
-			"float3 normal = 0.0f;"
-			"float lastWeight = 0.0f;"
-
-			"float blendWeights[4] = (float[4])i.blendWeights;"
-			"int idxAry[4] = (int[4])i.blendIndices;"
-
-			"for (int iBone = 0; iBone < numBones-1; iBone++) {"
-				"lastWeight = lastWeight + blendWeights[iBone];"
-				"pos += mul(i.pos, mWorldMatrixArray[idxAry[iBone]]) * blendWeights[iBone];"
-				"normal += mul(i.normal, mWorldMatrixArray[idxAry[iBone]]) * blendWeights[iBone];"
-			"}"
-			"lastWeight = 1.0f - lastWeight;"
-
-			"pos += (mul(i.pos, mWorldMatrixArray[idxAry[numBones-1]]) * lastWeight);"
-			"normal += (mul(i.normal, mWorldMatrixArray[idxAry[numBones-1]]) * lastWeight);"
-			"o.pos = mul(float4(pos.xyz, 1.0f), mViewProj);"
-
-			"normal = normalize(normal);"
-			"o.diffuse.xyz = materialAmbient.xyz + CalcDiffuse(normal) * materialDiffuse.xyz;"
-			"o.diffuse.w = materialAmbient.w * materialDiffuse.w;"
-
-			"o.tex0 = i.tex0;"
-			"o.fog = 1.0f - (o.pos.z - fogNear) / (fogFar - fogNear);"
-
-			"return o;"
-		"}"
-
-		"technique BasicTec {"
-			"pass P0 {"
-				"VertexShader = compile vs_2_0 DefaultTransform(4);"
-			"}"
-		"}";
-
 	const std::string ShaderSource::nameRender2D_ = "_HLSL_INTERNAL_RENDER2D";
 	const std::string ShaderSource::sourceRender2D_ =
 		"sampler samp0_ : register(s0);"
@@ -511,42 +443,49 @@ namespace directx {
 
 		{
 			//<source, name>
-			std::pair<const std::string*, const std::string*> listCreate[] = {
-				std::make_pair(&ShaderSource::sourceSkinnedMesh_, &ShaderSource::nameSkinnedMesh_),
+			std::vector<std::pair<const std::string*, const std::string*>> listCreate = {
 				std::make_pair(&ShaderSource::sourceRender2D_, &ShaderSource::nameRender2D_),
 				std::make_pair(&ShaderSource::sourceHwInstance2D_, &ShaderSource::nameHwInstance2D_),
 				std::make_pair(&ShaderSource::sourceHwInstance3D_, &ShaderSource::nameHwInstance3D_),
 				std::make_pair(&ShaderSource::sourceIntersectVisual1_, &ShaderSource::nameIntersectVisual1_),
 				std::make_pair(&ShaderSource::sourceIntersectVisual2_, &ShaderSource::nameIntersectVisual2_)
 			};
-			for (size_t iEff = 0U; iEff < sizeof(listCreate) / sizeof(*listCreate); ++iEff) {
-				const std::string* ptrSrc = listCreate[iEff].first;
+			for (size_t iEff = 0U; iEff < listCreate.size(); ++iEff) {
+				const std::string* source = listCreate[iEff].first;
+				const std::string* name = listCreate[iEff].second;
 
-				hr = D3DXCreateEffect(device, ptrSrc->c_str(), ptrSrc->size(), nullptr, nullptr, 0,
+				hr = D3DXCreateEffect(device, source->c_str(), source->size(), nullptr, nullptr, 0,
 					nullptr, &listEffect_[iEff], &error);
 				if (FAILED(hr)) {
-					std::string err = StringUtility::Format("RenderShaderLibrary: Shader load failed. [%s]\r\n\t%s",
-						listCreate[iEff].second->c_str(), reinterpret_cast<const char*>(error->GetBufferPointer()));
+					const char* strCompileError = "unknown error";
+					if (error)
+						strCompileError = reinterpret_cast<const char*>(error->GetBufferPointer());
+					std::string err = StringUtility::Format(
+						"RenderShaderLibrary: Shader compile failed. [%s]\r\n\t%s\r\n\t%s",
+						name->c_str(), DXGetErrorStringA(hr), strCompileError);
 					throw gstd::wexception(err);
 				}
 			}
 		}
-		listEffect_[1]->SetTechnique("Render");
+		listEffect_[0]->SetTechnique("Render");
 
 		{
-			std::pair<const D3DVERTEXELEMENT9*, std::string> listCreate[] = {
+			std::vector<std::pair<const D3DVERTEXELEMENT9*, std::string>> listCreate = {
 				std::make_pair(ELEMENTS_TLX, "ELEMENTS_TLX"),
 				std::make_pair(ELEMENTS_LX, "ELEMENTS_LX"),
 				std::make_pair(ELEMENTS_NX, "ELEMENTS_NX"),
-				std::make_pair(ELEMENTS_BNX, "ELEMENTS_BNX"),
 				std::make_pair(ELEMENTS_TLX_INSTANCED, "ELEMENTS_TLX_INSTANCED"),
 				std::make_pair(ELEMENTS_LX_INSTANCED, "ELEMENTS_LX_INSTANCED")
 			};
-			for (size_t iDecl = 0U; iDecl < sizeof(listCreate) / sizeof(*listCreate); ++iDecl) {
-				hr = device->CreateVertexDeclaration(listCreate[iDecl].first, &listDeclaration_[iDecl]);
+			for (size_t iDecl = 0U; iDecl < listCreate.size(); ++iDecl) {
+				const D3DVERTEXELEMENT9* elem = listCreate[iDecl].first;
+				const std::string& name = listCreate[iDecl].second;
+
+				hr = device->CreateVertexDeclaration(elem, &listDeclaration_[iDecl]);
 				if (FAILED(hr)) {
-					std::string err = StringUtility::Format("RenderShaderLibrary: CreateVertexDeclaration failed. (%s)",
-						listCreate[iDecl].second.c_str());
+					std::string err = StringUtility::Format(
+						"RenderShaderLibrary: CreateVertexDeclaration failed. [%s]\r\n\t%s",
+						name.c_str(), DXGetErrorStringA(hr));
 					throw gstd::wexception(err);
 				}
 			}
--- source/GcLib/directx/HLSL.hpp
+++ source/GcLib/directx/HLSL.hpp
@@ -7,9 +7,6 @@
 namespace directx {
 	class ShaderSource {
 	public:
-		static const std::string nameSkinnedMesh_;
-		static const std::string sourceSkinnedMesh_;
-
 		static const std::string nameRender2D_;
 		static const std::string sourceRender2D_;
 
@@ -44,29 +41,26 @@ namespace directx {
 		void OnLostDevice();
 		void OnResetDevice();
 
-		ID3DXEffect* GetSkinnedMeshShader() { return listEffect_[0]; }
-		ID3DXEffect* GetRender2DShader() { return listEffect_[1]; }
-		ID3DXEffect* GetInstancing2DShader() { return listEffect_[2]; }
-		ID3DXEffect* GetInstancing3DShader() { return listEffect_[3]; }
-		ID3DXEffect* GetIntersectVisualShader1() { return listEffect_[4]; }
-		ID3DXEffect* GetIntersectVisualShader2() { return listEffect_[5]; }
+		ID3DXEffect* GetRender2DShader() { return listEffect_[0]; }
+		ID3DXEffect* GetInstancing2DShader() { return listEffect_[1]; }
+		ID3DXEffect* GetInstancing3DShader() { return listEffect_[2]; }
+		ID3DXEffect* GetIntersectVisualShader1() { return listEffect_[3]; }
+		ID3DXEffect* GetIntersectVisualShader2() { return listEffect_[4]; }
 
 		IDirect3DVertexDeclaration9* GetVertexDeclarationTLX() { return listDeclaration_[0]; }
 		IDirect3DVertexDeclaration9* GetVertexDeclarationLX() { return listDeclaration_[1]; }
 		IDirect3DVertexDeclaration9* GetVertexDeclarationNX() { return listDeclaration_[2]; }
-		IDirect3DVertexDeclaration9* GetVertexDeclarationBNX() { return listDeclaration_[3]; }
-		IDirect3DVertexDeclaration9* GetVertexDeclarationInstancedTLX() { return listDeclaration_[4]; }
-		IDirect3DVertexDeclaration9* GetVertexDeclarationInstancedLX() { return listDeclaration_[5]; }
+		IDirect3DVertexDeclaration9* GetVertexDeclarationInstancedTLX() { return listDeclaration_[3]; }
+		IDirect3DVertexDeclaration9* GetVertexDeclarationInstancedLX() { return listDeclaration_[4]; }
 
 		D3DXMATRIX* GetArrayMatrix() { return arrayMatrix; }
 	private:
 		/*
-		 * 0 -> Skinned Mesh
-		 * 1 -> 2D Render
-		 * 2 -> 2D Hardware Instancing
-		 * 3 -> 3D Hardware Instancing
-		 * 4 -> Intersection visualizer (circle)
-		 * 5 -> Intersection visualizer (line)
+		 * 0 -> 2D Render
+		 * 1 -> 2D Hardware Instancing
+		 * 2 -> 3D Hardware Instancing
+		 * 3 -> Intersection visualizer (circle)
+		 * 4 -> Intersection visualizer (line)
 		 */
 		std::vector<ID3DXEffect*> listEffect_;
 
@@ -74,9 +68,8 @@ namespace directx {
 		 * 0 -> TLX
 		 * 1 -> LX
 		 * 2 -> NX
-		 * 3 -> BNX
-		 * 4 -> Instanced TLX
-		 * 5 -> Instanced LX
+		 * 3 -> Instanced TLX
+		 * 4 -> Instanced LX
 		 */
 		std::vector<IDirect3DVertexDeclaration9*> listDeclaration_/*of Independence*/;
 
