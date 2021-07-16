4687d4b78f2a82bea014bab0a022bf2d6c111ede
Natashi <32347635+Natashi@users.noreply.github.com>
2021-07-16 14:41:02 +0700

    Made make_unique recursive

--- source/GcLib/gstd/Script/Value.cpp
+++ source/GcLib/gstd/Script/Value.cpp
@@ -180,6 +180,8 @@ void value::make_unique() {
 	if (has_data() && kind == type_data::tk_array) {
 		if (p_array_value.use_count() == 1) return;
 		std::vector<value> vec = *p_array_value.get();
+		for (value& v : vec)
+			v.make_unique();
 		this->reset(type, vec);
 	}
 }
@@ -209,12 +211,12 @@ size_t value::length_as_array() const {
 const value& value::index_as_array(size_t i) const {
 	if (has_data() && kind == type_data::tk_array)
 		return p_array_value->at(i);
-	return value();
+	throw wexception("index_as_array: not an array");
 }
 value& value::index_as_array(size_t i) {
 	if (has_data() && kind == type_data::tk_array)
 		return p_array_value->at(i);
-	return value();
+	throw wexception("index_as_array: not an array");
 }
 std::vector<value>::iterator value::array_get_begin() const {
 	if (has_data() && kind == type_data::tk_array)
