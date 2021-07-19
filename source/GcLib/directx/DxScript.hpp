251b932a7eaba7c695b89fd3a98ed969e887e33e
Naudiz <31663251+nazjun@users.noreply.github.com>
2021-07-19 19:05:19 -0400

    :neck_pogo_deepfry:

--- source/GcLib/directx/DxScript.hpp
+++ source/GcLib/directx/DxScript.hpp
@@ -213,6 +213,7 @@ namespace directx {
 		DNH_FUNCAPI_DECL_(Func_IsIntersected_Line_Line);
 
 		DNH_FUNCAPI_DECL_(Func_IsIntersected_Circle_RegularPolygon);
+		DNH_FUNCAPI_DECL_(Func_IsIntersected_Circle_Ellipse);
 
 		//Color
 		DNH_FUNCAPI_DECL_(Func_ColorARGBToHex);
