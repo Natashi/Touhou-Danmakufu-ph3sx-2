a44cfbdd69732d6f8e4c69b9c4aefb347c2f1843
Naudiz <31663251+nazjun@users.noreply.github.com>
2021-08-14 20:44:01 -0400

    256 render priorities because I like to live large

--- source/GcLib/directx/DxObject.cpp
+++ source/GcLib/directx/DxObject.cpp
@@ -1289,7 +1289,7 @@ DWORD DxBinaryFileObject::Write(LPVOID data, size_t size) {
 DxScriptObjectManager::FogData DxScriptObjectManager::fogData_ = { false, 0xffffffff, 0, 0 };
 DxScriptObjectManager::DxScriptObjectManager() {
 	SetMaxObject(DEFAULT_CONTAINER_CAPACITY);
-	SetRenderBucketCapacity(101);
+	SetRenderBucketCapacity(256);
 
 	totalObjectCreateCount_ = 0U;
 }
