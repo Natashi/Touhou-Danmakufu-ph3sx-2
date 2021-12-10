b7a418c628c7a0e85bc97eb741646937d002eccc
Natashi <32347635+Natashi@users.noreply.github.com>
2021-12-10 22:00:30 +0700

    Update version string

--- source/GcLib/gstd/GstdConstant.hpp
+++ source/GcLib/gstd/GstdConstant.hpp
@@ -4,7 +4,7 @@
 constexpr const int STANDARD_FPS = 60;
 
 const std::wstring DNH_EXE_NAME = L"th_dnh_ph3sx.exe";
-const std::wstring DNH_VERSION = L"v1.31a-pre";
+const std::wstring DNH_VERSION = L"v1.31a";
 
 constexpr const uint64_t _GAME_VERSION_RESERVED = /*e*/621;		//OWO!!!!!
 constexpr const uint64_t _GAME_VERSION_MAJOR = 1;
