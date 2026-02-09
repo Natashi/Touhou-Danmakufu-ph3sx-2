f432960056dd1776225b3e5920df41a141615ae5
Natashi <32347635+Natashi@users.noreply.github.com>
2026-02-09 15:54:33 +0700

    Create ByteUtility::Cast for safe bit casting

--- source/GcLib/directx/DirectGraphics.cpp
+++ source/GcLib/directx/DirectGraphics.cpp
@@ -696,8 +696,8 @@ void DirectGraphics::SetVertexFog(bool bEnable, D3DCOLOR color, float start, flo
 
 	pDevice_->SetRenderState(D3DRS_FOGCOLOR, color);
 	pDevice_->SetRenderState(D3DRS_FOGVERTEXMODE, D3DFOG_LINEAR);
-	pDevice_->SetRenderState(D3DRS_FOGSTART, *(DWORD*)(&start));
-	pDevice_->SetRenderState(D3DRS_FOGEND, *(DWORD*)(&end));
+	pDevice_->SetRenderState(D3DRS_FOGSTART, ByteUtility::Cast<DWORD>(start));
+	pDevice_->SetRenderState(D3DRS_FOGEND, ByteUtility::Cast<DWORD>(end));
 
 	stateFog_.bEnable = bEnable;
 	stateFog_.color = ColorAccess::ToVec4Normalized(color, ColorAccess::PERMUTE_RGBA);
